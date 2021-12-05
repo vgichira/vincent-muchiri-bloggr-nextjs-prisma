93ee4d7de38f3edce1faf55f13d3c70c74eb3df3
Vincent Gichira <vgichira39@gmail.com>
2021-12-05 17:34:43 +0300

    fixed prisma client bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient
+const prismaClientPropertyName = `__prevent-name-collision__prisma`;
+type GlobalThisWithPrismaClient = typeof globalThis & {
+	[prismaClientPropertyName]: PrismaClient;
+};
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
+const getPrismaClient = () => {
+	if (process.env.NODE_ENV === `production`) {
+		return new PrismaClient();
+	} else {
+		const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
+		if (!newGlobalThis[prismaClientPropertyName]) {
+			newGlobalThis[prismaClientPropertyName] = new PrismaClient();
+		}
+		return newGlobalThis[prismaClientPropertyName];
+	}
+};
 
-    prisma = global.prisma;
-}
+const prisma = getPrismaClient();
 
 export default prisma;
